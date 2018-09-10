83e3eecfeb5a5af391b5492a3666d7084c9a5682
dariober <dario.beraldi@gmail.com>
2018-09-10 11:16:16 +0100

    Do not round transcript lengths to kilo-bases
    We report transcript lengths in base-pairs, i.e. as integers instead of
    kilobases (as floats).

--- ensembldb/main.py
+++ ensembldb/main.py
@@ -35,7 +35,7 @@ class Transcript(object):
         if self.STRAND == '1': ret = '+/'
         else: ret = '-/'
         cdna = self.getcDNALength()
-        return ret+str(round((self.POSEND-self.POS+1) / 1000, 1))+'kb/'+str(len(self.EXONS))+'/'+str(round(cdna / 1000, 1))+'kb/'+str(self.getProteinLength())
+        return ret+str(self.POSEND-self.POS+1)+'bp/'+str(len(self.EXONS))+'/'+str(cdna)+'bp/'+str(self.getProteinLength())
 
 
     # Get cDNA length of the transcript
